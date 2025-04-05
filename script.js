8b85e949909c1336a86aef1bd1b5523153c4d7b4
Yana <sxxqvs@gmail.com>
2025-04-05 18:59:02 +0300

    added a sense of weather, wind, humidity, pressure, maximum and minimum temperature during the day, sunrise and sunset

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-WEATHER_API_ENDPOINT = "http://api.weatherapi.com/v1/current.json?key=927baf2d296442549b3122200250304&q=";
-WEATHER_DATA_ENDPOINT = "";
+WEATHER_API_ENDPOINT = "http://api.weatherapi.com/v1/forecast.json?days=5&key=927baf2d296442549b3122200250304&q=";
+// WEATHER_API_FORECAST = "http://api.weatherapi.com/v1/forecast.json?key=927baf2d296442549b3122200250304&q=";
+// LOCATION_API = "https://api.opencagedata.com/geocode/v1/json?q=52.5432379%2C+13.4142133&key=6ccabc59726c4fd981540c5c27b5d48d";
 
+fetch(WEATHER_API_ENDPOINT + "tokyo").then(response => response.json()).then(data => {
+    console.log(data)
+})
 
-fetch(WEATHER_API_ENDPOINT + "Tokyo")
+const cities = [
+    {name: "Tokyo", selector: ".left-fever1"},
+    {name: "Dubai", selector: ".left-fever2"},
+    {name: "Paris", selector: ".left-fever3"},
+    {name: "Bali", selector: ".left-fever4"}
+]
+
+function fetchTemperature(city, selector) {
+    fetch(WEATHER_API_ENDPOINT + city)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         const temperature = data.current.temp_c;
-        document.querySelector(".left-fever1").textContent = temperature + "°C"
+        document.querySelector(selector).textContent = temperature + "°C"
     })
-
-fetch(WEATHER_API_ENDPOINT + "Dubai")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const temperature = data.current.temp_c;
-        document.querySelector(".left-fever2").textContent = temperature + "°C"
-    })
-
-fetch(WEATHER_API_ENDPOINT + "Paris")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const temperature = data.current.temp_c;
-        document.querySelector(".left-fever3").textContent = temperature + "°C"
-    })
-
-
-fetch(WEATHER_API_ENDPOINT + "Bali")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const temperature = data.current.temp_c;
-        document.querySelector(".left-fever4").textContent = temperature + "°C"
-    })
+}
 
 function findUserLocation(city) {
     fetch(WEATHER_API_ENDPOINT + city)
@@ -44,8 +31,16 @@ function findUserLocation(city) {
         const country = data.location.country;
         const temperature = data.current.temp_c;
         const weatherIcon = data.current.condition.icon;
+        const feelslikeC = data.current.feelslike_c;
+        const humidity = data.current.humidity;
+        const pressure = data.current.pressure_mb;
+        const wind = data.current.gust_kph;
+        const maxTemp = data.forecast.forecastday[0].day.maxtemp_c;
+        const minTemp = data.forecast.forecastday[0].day.mintemp_c;
+        const sunrise = data.forecast.forecastday[0].astro.sunrise;
+        const sunset = data.forecast.forecastday[0].astro.sunset;
 
-
+        console.log(feelslikeC)
         document.getElementById("cityText").textContent = name;
         document.getElementById("leftCityText").textContent = name;
         document.getElementById("countryText").textContent = country;
@@ -53,6 +48,14 @@ function findUserLocation(city) {
         document.getElementById("feverText").textContent = temperature + "°C";
         document.getElementById("leftFeverText").textContent = temperature + "°C";
         document.querySelector(".weather-icon").src = weatherIcon;
+        document.querySelector(".feelslike-c-text").textContent = feelslikeC;
+        document.querySelector(".gusts-kph-text").textContent = wind;
+        document.querySelector(".humidity-text").textContent = humidity;
+        document.querySelector(".pressure-text").textContent = pressure;
+        document.querySelector(".max-temp-text").textContent = maxTemp + "°";
+        document.querySelector(".min-temp-text").textContent = minTemp + "°";
+        document.querySelector(".sunrise-text").textContent = sunrise;
+        document.querySelector(".sunset-text").textContent = sunset;
     });
 }
 
@@ -74,3 +77,12 @@ document.querySelectorAll(".city-and-fever-group-list").forEach(cityElement => {
         findUserLocation(city)
     })
 })
+
+
+function updateAllTemperatures() {
+    cities.forEach(({name, selector}) => fetchTemperature(name, selector));
+}
+
+updateAllTemperatures();
+
+setInterval(updateAllTemperatures, 60000)
