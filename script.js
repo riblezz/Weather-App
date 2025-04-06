eb7c9852ccd3d40da66493ca521d7861455cdf9d
Yana <sxxqvs@gmail.com>
2025-04-06 21:19:12 +0300

    added weather during the day, country-specific date and new icons

--- script.js
+++ script.js
@@ -40,6 +40,108 @@ function findUserLocation(city) {
         const sunrise = data.forecast.forecastday[0].astro.sunrise;
         const sunset = data.forecast.forecastday[0].astro.sunset;
 
+
+
+
+
+        const iconDownZero = data.forecast.forecastday[0].hour[0].condition.icon;
+        const tempDownZero = data.forecast.forecastday[0].hour[0].temp_c;
+        const windDownZero = data.forecast.forecastday[0].hour[0].gust_kph;
+
+        const iconDownOne = data.forecast.forecastday[0].hour[1].condition.icon;
+        const tempDownOne = data.forecast.forecastday[0].hour[1].temp_c;
+        const windDownOne = data.forecast.forecastday[0].hour[1].gust_kph;
+
+        const iconDownTwo = data.forecast.forecastday[0].hour[2].condition.icon;
+        const tempDownTwo = data.forecast.forecastday[0].hour[2].temp_c;
+        const windDownTwo = data.forecast.forecastday[0].hour[2].gust_kph;
+
+        const iconDownThree = data.forecast.forecastday[0].hour[3].condition.icon;
+        const tempDownThree = data.forecast.forecastday[0].hour[3].temp_c;
+        const windDownThree = data.forecast.forecastday[0].hour[3].gust_kph;
+
+        const iconDownFour = data.forecast.forecastday[0].hour[4].condition.icon;
+        const tempDownFour= data.forecast.forecastday[0].hour[4].temp_c;
+        const windDownFour = data.forecast.forecastday[0].hour[4].gust_kph;
+
+        const iconDownFive = data.forecast.forecastday[0].hour[5].condition.icon;
+        const tempDownFive = data.forecast.forecastday[0].hour[5].temp_c;
+        const windDownFive = data.forecast.forecastday[0].hour[5].gust_kph;
+
+        const iconDownSix = data.forecast.forecastday[0].hour[6].condition.icon;
+        const tempDownSix = data.forecast.forecastday[0].hour[6].temp_c;
+        const windDownSix = data.forecast.forecastday[0].hour[6].gust_kph;
+
+        const iconDownSeven = data.forecast.forecastday[0].hour[7].condition.icon;
+        const tempDownSeven = data.forecast.forecastday[0].hour[7].temp_c;
+        const windDownSeven = data.forecast.forecastday[0].hour[7].gust_kph;
+
+        const iconDownEight = data.forecast.forecastday[0].hour[8].condition.icon;
+        const tempDownEight = data.forecast.forecastday[0].hour[8].temp_c;
+        const windDownEight = data.forecast.forecastday[0].hour[8].gust_kph;
+
+        const iconDownNine = data.forecast.forecastday[0].hour[9].condition.icon;
+        const tempDownNine = data.forecast.forecastday[0].hour[9].temp_c;
+        const windDownNine = data.forecast.forecastday[0].hour[9].gust_kph;
+
+        const iconDownTen = data.forecast.forecastday[0].hour[10].condition.icon;
+        const tempDownTen = data.forecast.forecastday[0].hour[10].temp_c;
+        const windDownTen = data.forecast.forecastday[0].hour[10].gust_kph;
+
+        const iconDownEleven = data.forecast.forecastday[0].hour[11].condition.icon;
+        const tempDownEleven = data.forecast.forecastday[0].hour[11].temp_c;
+        const windDownEleven = data.forecast.forecastday[0].hour[11].gust_kph;
+
+        const iconDownTwelve = data.forecast.forecastday[0].hour[12].condition.icon;
+        const tempDownTwelve = data.forecast.forecastday[0].hour[12].temp_c;
+        const windDownTwelve = data.forecast.forecastday[0].hour[12].gust_kph;
+
+        const iconDownThirteen = data.forecast.forecastday[0].hour[13].condition.icon;
+        const tempDownThirteen = data.forecast.forecastday[0].hour[13].temp_c;
+        const windDownThirteen = data.forecast.forecastday[0].hour[13].gust_kph;
+
+        const iconDownFourteen = data.forecast.forecastday[0].hour[14].condition.icon;
+        const tempDownFourteen = data.forecast.forecastday[0].hour[14].temp_c;
+        const windDownFourteen = data.forecast.forecastday[0].hour[14].gust_kph;
+
+        const iconDownFifteen = data.forecast.forecastday[0].hour[15].condition.icon;
+        const tempDownFifteen = data.forecast.forecastday[0].hour[15].temp_c;
+        const windDownFifteen = data.forecast.forecastday[0].hour[15].gust_kph;
+
+        const iconDownSixteen = data.forecast.forecastday[0].hour[16].condition.icon;
+        const tempDownSixteen = data.forecast.forecastday[0].hour[16].temp_c;
+        const windDownSixteen = data.forecast.forecastday[0].hour[16].gust_kph;
+
+        const iconDownSeventeen = data.forecast.forecastday[0].hour[17].condition.icon;
+        const tempDownSeventeen = data.forecast.forecastday[0].hour[17].temp_c;
+        const windDownSeventeen = data.forecast.forecastday[0].hour[17].gust_kph;
+
+        const iconDownEighteen = data.forecast.forecastday[0].hour[18].condition.icon;
+        const tempDownEighteen = data.forecast.forecastday[0].hour[18].temp_c;
+        const windDownEighteen = data.forecast.forecastday[0].hour[18].gust_kph;
+
+        const iconDownNineteen = data.forecast.forecastday[0].hour[19].condition.icon;
+        const tempDownNineteen = data.forecast.forecastday[0].hour[19].temp_c;
+        const windDownNineteen = data.forecast.forecastday[0].hour[19].gust_kph;
+
+        const iconDownTwenty = data.forecast.forecastday[0].hour[20].condition.icon;
+        const tempDownTwenty = data.forecast.forecastday[0].hour[20].temp_c;
+        const windDownTwenty = data.forecast.forecastday[0].hour[20].gust_kph;
+
+        const iconDownTwentyOne = data.forecast.forecastday[0].hour[21].condition.icon;
+        const tempDownTwentyOne = data.forecast.forecastday[0].hour[21].temp_c;
+        const windDownTwentyOne = data.forecast.forecastday[0].hour[21].gust_kph;
+
+        const iconDownTwentyTwo = data.forecast.forecastday[0].hour[22].condition.icon;
+        const tempDownTwentyTwo = data.forecast.forecastday[0].hour[22].temp_c;
+        const windDownTwentyTwo = data.forecast.forecastday[0].hour[22].gust_kph;
+
+        const iconDownTwentyThree = data.forecast.forecastday[0].hour[23].condition.icon;
+        const tempDownTwentyThree = data.forecast.forecastday[0].hour[23].temp_c;
+        const windDownTwentyThree = data.forecast.forecastday[0].hour[23].gust_kph;
+
+        const time = data.location.localtime;
+
         console.log(feelslikeC)
         document.getElementById("cityText").textContent = name;
         document.getElementById("leftCityText").textContent = name;
@@ -56,6 +158,104 @@ function findUserLocation(city) {
         document.querySelector(".min-temp-text").textContent = minTemp + "°";
         document.querySelector(".sunrise-text").textContent = sunrise;
         document.querySelector(".sunset-text").textContent = sunset;
+
+        document.querySelector(".icon-down-double-zero").src = iconDownZero;
+        document.querySelector(".temp-down-double-zero").textContent = tempDownZero;
+        document.querySelector(".wind-down-double-zero").textContent = windDownZero;
+
+        document.querySelector(".icon-down-zero-one").src = iconDownOne;
+        document.querySelector(".temp-down-zero-one").textContent = tempDownOne;
+        document.querySelector(".wind-down-zero-one").textContent = windDownOne;
+
+        document.querySelector(".icon-down-zero-two").src = iconDownTwo;
+        document.querySelector(".temp-down-zero-two").textContent = tempDownTwo;
+        document.querySelector(".wind-down-zero-two").textContent = windDownTwo;
+
+        document.querySelector(".icon-down-zero-three").src = iconDownThree;
+        document.querySelector(".temp-down-zero-three").textContent = tempDownThree;
+        document.querySelector(".wind-down-zero-three").textContent = windDownThree;
+
+        document.querySelector(".icon-down-zero-four").src = iconDownFour;
+        document.querySelector(".temp-down-zero-four").textContent = tempDownFour;
+        document.querySelector(".wind-down-zero-four").textContent = windDownFour;
+
+        document.querySelector(".icon-down-zero-five").src = iconDownFive;
+        document.querySelector(".temp-down-zero-five").textContent = tempDownFive;
+        document.querySelector(".wind-down-zero-five").textContent = windDownFive;
+
+        document.querySelector(".icon-down-zero-six").src = iconDownSix;
+        document.querySelector(".temp-down-zero-six").textContent = tempDownSix;
+        document.querySelector(".wind-down-zero-six").textContent = windDownSix;
+
+        document.querySelector(".icon-down-zero-seven").src = iconDownSeven;
+        document.querySelector(".temp-down-zero-seven").textContent = tempDownSeven;
+        document.querySelector(".wind-down-zero-seven").textContent = windDownSeven;
+
+        document.querySelector(".icon-down-zero-eight").src = iconDownEight;
+        document.querySelector(".temp-down-zero-eight").textContent = tempDownEight;
+        document.querySelector(".wind-down-zero-eight").textContent = windDownEight;
+
+        document.querySelector(".icon-down-zero-nine").src = iconDownNine;
+        document.querySelector(".temp-down-zero-nine").textContent = tempDownNine;
+        document.querySelector(".wind-down-zero-nine").textContent = windDownNine;
+
+        document.querySelector(".icon-down-ten").src = iconDownTen;
+        document.querySelector(".temp-down-ten").textContent = tempDownTen;
+        document.querySelector(".wind-down-ten").textContent = windDownTen;
+
+        document.querySelector(".icon-down-eleven").src = iconDownEleven;
+        document.querySelector(".temp-down-eleven").textContent = tempDownEleven;
+        document.querySelector(".wind-down-eleven").textContent = windDownEleven;
+
+        document.querySelector(".icon-down-twelve").src = iconDownTwelve;
+        document.querySelector(".temp-down-twelve").textContent = tempDownTwelve;
+        document.querySelector(".wind-down-twelve").textContent = windDownTwelve;
+
+        document.querySelector(".icon-down-zero-one-pm").src = iconDownThirteen;
+        document.querySelector(".temp-down-zero-one-pm").textContent = tempDownThirteen;
+        document.querySelector(".wind-down-zero-one-pm").textContent = windDownThirteen;
+
+        document.querySelector(".icon-down-zero-two-pm").src = iconDownFourteen;
+        document.querySelector(".temp-down-zero-two-pm").textContent = tempDownFourteen;
+        document.querySelector(".wind-down-zero-two-pm").textContent = windDownFourteen;
+
+        document.querySelector(".icon-down-zero-three-pm").src = iconDownFifteen;
+        document.querySelector(".temp-down-zero-three-pm").textContent = tempDownFifteen;
+        document.querySelector(".wind-down-zero-three-pm").textContent = windDownFifteen;
+
+        document.querySelector(".icon-down-zero-four-pm").src = iconDownSixteen;
+        document.querySelector(".temp-down-zero-four-pm").textContent = tempDownSixteen;
+        document.querySelector(".wind-down-zero-four-pm").textContent = windDownSixteen;
+
+        document.querySelector(".icon-down-zero-five-pm").src = iconDownSeventeen;
+        document.querySelector(".temp-down-zero-five-pm").textContent = tempDownSeventeen;
+        document.querySelector(".wind-down-zero-five-pm").textContent = windDownSeventeen;
+
+        document.querySelector(".icon-down-zero-six-pm").src = iconDownEighteen;
+        document.querySelector(".temp-down-zero-six-pm").textContent = tempDownEighteen;
+        document.querySelector(".wind-down-zero-six-pm").textContent = windDownEighteen;
+
+        document.querySelector(".icon-down-zero-seven-pm").src = iconDownNineteen;
+        document.querySelector(".temp-down-zero-seven-pm").textContent = tempDownNineteen;
+        document.querySelector(".wind-down-zero-seven-pm").textContent = windDownNineteen;
+
+        document.querySelector(".icon-down-zero-eight-pm").src = iconDownTwenty;
+        document.querySelector(".temp-down-zero-eight-pm").textContent = tempDownTwenty;
+        document.querySelector(".wind-down-zero-eight-pm").textContent = windDownTwenty;
+
+        document.querySelector(".icon-down-zero-nine-pm").src = iconDownTwentyOne;
+        document.querySelector(".temp-down-zero-nine-pm").textContent = tempDownTwentyOne;
+        document.querySelector(".wind-down-zero-nine-pm").textContent = windDownTwentyOne;
+
+        document.querySelector(".icon-down-ten-pm").src = iconDownTwentyTwo;
+        document.querySelector(".temp-down-ten-pm").textContent = tempDownTwentyTwo;
+        document.querySelector(".wind-down-ten-pm").textContent = windDownTwentyTwo;
+
+        document.querySelector(".icon-down-eleven-pm").src = iconDownTwentyThree;
+        document.querySelector(".temp-down-eleven-pm").textContent = tempDownTwentyThree;
+        document.querySelector(".wind-down-eleven-pm").textContent = windDownTwentyThree;
+
+        document.querySelector(".data-today").textContent = time.slice(0,10);
     });
 }
 
@@ -85,4 +285,5 @@ function updateAllTemperatures() {
 
 updateAllTemperatures();
 
-setInterval(updateAllTemperatures, 60000)
+setInterval(updateAllTemperatures, 100000);
+
